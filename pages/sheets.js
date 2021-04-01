594c7d455626c2a0201f4b71e3f4966807c6ffaf
Helmut Granda <contact@helmutgranda.com>
2021-03-31 22:50:30 -0500

    Adding sheets page.

---
+++ pages/sheets.js
@@ -0,0 +1,16 @@
+import GlobalHeader from "../components/GlobalHeader"
+import styles from '../styles/Sheets.module.scss'
+
+export default function Sheets() {
+
+  return (
+    <>
+    <GlobalHeader></GlobalHeader>
+    <div className={styles.container}>
+      Sheets content
+    </div>
+    
+    </>
+  )
+
+}
